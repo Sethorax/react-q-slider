1d4018464bd2d86b84d5178567d6e4fc43e8f3ca
Sethorax <info@sethorax.com>
2018-03-19 19:40:58 +0100

    Added proper styles to the arrows

--- src/components/slider-navigation.jsx
+++ src/components/slider-navigation.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 class SliderNavigation extends React.Component {
     render() {
-        const NextArrow = this.props.nextArrow ? React.cloneElement(this.props.nextArrow, { onClick: this.props.onNextClick }) : <button onClick={this.props.onNextClick}>Next</button>;
-        const PrevArrow = this.props.prevArrow ? React.cloneElement(this.props.prevArrow, { onClick: this.props.onPrevClick }) : <button onClick={this.props.onPrevClick}>Prev</button>;
+        const NextArrow = this.props.nextArrow ? React.cloneElement(this.props.nextArrow, { onClick: this.props.onNextClick }) : <div className="q-slider__arrow q-slider__arrow_next" onClick={this.props.onNextClick}></div>;
+        const PrevArrow = this.props.prevArrow ? React.cloneElement(this.props.prevArrow, { onClick: this.props.onPrevClick }) : <div className="q-slider__arrow q-slider__arrow_prev" onClick={this.props.onPrevClick}></div>;
 
         return (
             <div className="q-slider__navigation">
